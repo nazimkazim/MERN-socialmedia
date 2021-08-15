0a046dae2b24ca684d690403eafb12626d44fb7f
Nazim <nazik_111@mail.ru>
2021-08-15 14:08:34 +0600

    add complete feed

--- front-end/src/components/RightBar/RightBar.jsx
+++ front-end/src/components/RightBar/RightBar.jsx
@@ -2,5 +2,53 @@ import React from 'react';
 import './rightbar.css';
 
 export default function RightBar() {
-  return <div className='rightbar'>Right bar</div>;
+  return (
+    <div className='rightbar'>
+      <div className='rightBarWrapper'>
+        <div className='birthdayContainer'>
+          <img className='birthdayImg' src='assets/gift.png' alt='' />
+          <span className='birthdayText'>
+            <b>Pola Foster</b> and 3 others friends have a birthday today
+          </span>
+        </div>
+        <img className='rightbarAd' src='/assets/ad.png' alt='' />
+        <h4 className='rightbarTitle'>Online friends</h4>
+        <ul className='friendList'>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                className='rightbarProfileImg'
+                src='assets/person/3.jpeg'
+                alt=''
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                className='rightbarProfileImg'
+                src='assets/person/3.jpeg'
+                alt=''
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                className='rightbarProfileImg'
+                src='assets/person/3.jpeg'
+                alt=''
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>John Carter</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
--- front-end/src/components/post/Post.jsx
+++ front-end/src/components/post/Post.jsx
@@ -1,26 +1,37 @@
 import { MoreVert } from '@material-ui/icons';
 import React from 'react';
+import './post.css';
+import { Users } from '../../dummyData';
 
-export default function Post() {
+export default function Post({ post }) {
+  const user = Users.find((u) => u.id === post.userId);
   return (
     <div className='post'>
       <div className='postWrapper'>
         <div className='postTop'>
           <div className='postTopLeft'>
-            {/* <img
-              className='postProfileImg'
-              src='/assets/person/1.jpeg'
-              alt=''
-            /> */}
-            <span className='postUserName'>Nazim</span>
-            <span className='postDate'>5 mins ago</span>
+            <img className='postProfileImg' src={user?.profilePicture} alt='' />
+            <span className='postUserName'>{user?.username}</span>
+            <span className='postDate'>{post?.data}</span>
           </div>
           <div className='postTopRight'>
             <MoreVert />
           </div>
         </div>
-        <div className='postCenter'></div>
-        <div className='postBottom'></div>
+        <div className='postCenter'>
+          <span className='postText'>{post?.desc}</span>
+          <img className='postImg' src={post?.photo} alt='' />
+        </div>
+        <div className='postBottom'>
+          <div className='postBottomLeft'>
+            <img className='likeIcon' src='assets/like.png' alt='' />
+            <img className='likeIcon' src='assets/heart.png' alt='' />
+            <span className='postLikeCounter'>{post?.like} people like it</span>
+          </div>
+          <div className='postBottomRight'>
+            <span className='postCommentText'>{post?.comment} comments</span>
+          </div>
+        </div>
       </div>
     </div>
   );
