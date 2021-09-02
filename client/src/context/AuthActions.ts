8cba1890a5eaea33bb99523edcfba733e3427f1d
Nazim <nazik_111@mail.ru>
2021-09-02 11:54:30 +0600

    add remove ts from authContext

--- client/src/context/AuthActions.ts
+++ client/src/context/AuthActions.ts
@@ -1,1 +1,15 @@
-export { }
+import { UserObj } from '../interfaces'
+
+export const LoginStart = () => ({
+  type: "LOGIN_START",
+});
+
+export const LoginSuccess = (user: UserObj) => ({
+  type: "LOGIN_SUCCESS",
+  payload: user,
+});
+
+export const LoginFailure = (error: string) => ({
+  type: "LOGIN_FAILURE",
+  payload: error
+});
--- client/src/context/AuthReducer.ts
+++ client/src/context/AuthReducer.ts
@@ -1,1 +1,50 @@
-export { }
+import { UserObj } from '../interfaces';
+
+
+export interface InitialState {
+  user: null;
+  isFetching: boolean;
+  error: boolean;
+}
+
+const INITIAL_STATE: InitialState = {
+  user: null,
+  isFetching: false,
+  error: false,
+};
+
+type AppState = typeof INITIAL_STATE;
+
+
+type Action =
+  { type: "LOGIN_START" } |
+  { type: "LOGIN_SUCCESS"; payload: UserObj } |
+  { type: "LOGIN_FAILURE" }
+
+
+const AuthReducer = (state: AppState, action: Action) => {
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        user: null,
+        isFetching: true,
+        error: false,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        user: action.payload,
+        isFetching: false,
+        error: false,
+      };
+    case "LOGIN_FAILURE":
+      return {
+        user: null,
+        isFetching: false,
+        error: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default AuthReducer;
