179c5c402ba757365063d49b564231c7543d4519
Nazim <nazik_111@mail.ru>
2021-09-10 10:30:11 +0600

    almost completed image upload

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const dotnev = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const multer = require('multer');
+const path = require("path");
 
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
@@ -17,9 +19,29 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
   console.log('Connected to md');
 });
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
